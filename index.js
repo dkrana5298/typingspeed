0e1a77e3e3b2a14bff58b52d3b1fb758619a1ed7
Devendra Kumar <85422818+dkrana5298@users.noreply.github.com>
2021-12-12 15:59:55 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,50 @@
+var container=document.getElementById('container');
+var right=document.getElementById('right');
+function countChars(container,wpm){
+    var text=document.getElementById('container').value;
+    var word=text.split(" ");
+    var count=0;
+    for(var i=0;i<word.length;i++){
+       if(word[i]==""){
+       count +=0;
+       }
+       else {
+          count +=1;
+       }
+    }
+    document.getElementById('wpm').innerHTML=count;
+    // console.log(count)
+ }
+ 
+ var interval;
+ var second=0;
+ function timer(){
+   clearInterval(interval);
+  interval=setInterval(startTimer, 1000);
+  
+ }
+function startTimer(){
+    second++;
+    if(second < 10){
+        document.getElementById('timer').innerHTML="0" + second + " sec..";
+    }
+    if(second > 9){
+        document.getElementById('timer').innerHTML=second + " sec..";
+    }
+   if(second===10){
+       document.getElementById('average').innerHTML=
+       "your speed there: ";
+       right.style.display= "block";
+       right.style.display= "inline";
+     container.style.display= "none";
+     clearInterval(interval)
+     
+   }
+}
+// var counter=countChars();
+// var secondcount=startTimer();
+// console.log(count)
+//  var average;
+//  var time=second/60;
+//   average=((count)/5)/timer;
+//   document.getElementById('average').innerHTML=average;
